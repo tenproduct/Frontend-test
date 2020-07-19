f0315977877f6e2089d5e50f233d76fac2fc4e38
Tamas Cseh <tamas.cseh@cselt.hu>
2020-07-19 10:46:53 +0200

    Update angular material and cdk to version 9

--- src/app/modules/star-wars/star-wars.module.ts
+++ src/app/modules/star-wars/star-wars.module.ts
@@ -5,7 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { StarWarsRoutingModule } from './star-wars-routing.module';
 import * as fromStarWars from './state/star-wars.reducer';
