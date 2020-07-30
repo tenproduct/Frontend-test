dd237fc9f5744b9927aa02263d38e1c03505fb50
birodani3 <birodani3@gmail.com>
2020-07-30 18:29:45 +0200

    Add linter rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Star Wars App!';
+    public title = 'Star Wars App!';
 }
