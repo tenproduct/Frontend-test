64b03eb01deb03e1a5b7b297c181f93feae22f5d
Norbert Bagi <bagi.norbert@gmail.com>
2020-10-20 10:00:43 +0200

    Add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+import { CharacterComponent } from './components/character/character.component';
+import { SearchComponent } from './components/search/search.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    SearchComponent,
+    CharactersListComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
