483f691c1350929cf159af187b909082f34f1e4d
Laszlo Doczi <doczi.laszlo@gmail.com>
2021-09-09 21:22:42 +0200

    Add unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,91 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { cold } from 'jasmine-marbles';
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { SortComponent } from './components/sort/sort.component';
+import { SwCardComponent } from './components/sw-card/sw-card.component';
+import { Sort, SwapiResponse, SWPerson } from './models';
+import { reducer, rootStateKey } from './state/app.state';
+import { SwapiService } from './services';
+import { Observable, of } from 'rxjs';
+import { AppEffects } from './state/app.effects';
+import { MOCK_PEOPLE } from './test';
+
+const MOCK_RESPONSE = { count: 2, results: MOCK_PEOPLE, next: 'http://mock.api?page=2', previous: null };
+
+class MockSwapiService extends SwapiService {
+    getPeople(page: number = 1, search: string = ''): Observable<SwapiResponse> {
+        return of<SwapiResponse>(MOCK_RESPONSE);
+    }
+}
 
 describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [AppComponent],
-            imports: [MatToolbarModule]
+            declarations: [AppComponent, SearchComponent, SortComponent, SwCardComponent],
+            imports: [
+                MatToolbarModule,
+                StoreModule.forRoot({ [rootStateKey]: reducer }),
+                EffectsModule.forRoot([AppEffects]),
+                HttpClientTestingModule
+            ],
+            providers: [{ provide: SwapiService, useClass: MockSwapiService }]
         }).compileComponents();
     }));
 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        component.ngOnInit();
+    });
+
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+        expect(component).toBeTruthy();
+    });
+
+    describe('sort', () => {
+        it('by A-Z', () => {
+            component.onSortChange(Sort['A-Z']);
+            const expected = cold('(a)', { a: [MOCK_PEOPLE[1], MOCK_PEOPLE[0]] });
+
+            expect(component.people$).toBeObservable(expected);
+        });
+
+        it('by Z-A', () => {
+            component.onSortChange(Sort['Z-A']);
+            const expected = cold('(a)', { a: [MOCK_PEOPLE[0], MOCK_PEOPLE[1]] });
+
+            expect(component.people$).toBeObservable(expected);
+        });
+
+        it('by Male', () => {
+            component.onSortChange(Sort.Male);
+            const expected = cold('(a)', { a: [MOCK_PEOPLE[0], MOCK_PEOPLE[1]] });
+
+            expect(component.people$).toBeObservable(expected);
+        });
+
+        it('by Female', () => {
+            component.onSortChange(Sort.Female);
+            const expected = cold('(a)', { a: [MOCK_PEOPLE[1], MOCK_PEOPLE[0]] });
+
+            expect(component.people$).toBeObservable(expected);
+        });
+    });
+
+    it('should load more', () => {
+        component.onLoadMore();
+        const expected = cold('(a)', { a: [MOCK_PEOPLE[1], MOCK_PEOPLE[1], MOCK_PEOPLE[0], MOCK_PEOPLE[0]] });
+
+        expect(component.people$).toBeObservable(expected);
     });
 });
--- src/app/services/swapi.service.spec.ts
+++ src/app/services/swapi.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SwapiService } from './swapi.service';
 
 describe('SwapiService', () => {
-  let service: SwapiService;
+    let service: SwapiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SwapiService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        service = TestBed.inject(SwapiService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
